d35af7632f3310ccfab00e4952d6b0cf68a2cdcc
Joy Nichols <joynichols@joys-air.lan>
2022-10-06 21:15:41 -0400

    filled out all providers

---
+++ client/src/providers/AuthProvider.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export const AuthContext = React.createContext();
+export const AuthConsumer = AuthContext.Consumer;
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [errors, setErrors] = useState(null)
+
+  const navigate = useNavigate()
+
+  const handleRegister = (user) => {
+    axios.post('/api/auth', user )
+      .then( res => {
+        setUser(res.data.data)
+        navigate('/plants')
+      })
+      .catch( err => {
+        console.log(err)
+        setErrors({ 
+          variant: 'danger',
+          msg: err.response.data.errors.full_messages[0]
+        })
+      })
+  }
+
+  const handleLogin = (user) => {
+    axios.post('/api/auth/sign_in', user)
+      .then( res => {
+        setUser(res.data.data)
+        navigate('/plants')
+      })
+      .catch( err => {
+        console.log(err)
+        setErrors({ 
+          variant: 'danger',
+          msg: err.response.data.errors[0]
+        })
+      })
+  }
+
+  const handleLogout = () => {
+    axios.delete('/api/auth/sign_out')
+      .then( res => {
+        setUser(null)
+        navigate('/login')
+      })
+      .catch( err => {
+        console.log(err)
+        setErrors({ 
+          variant: 'danger',
+          msg: err.response.data.errors[0]
+        })
+      })
+  }
+  
+  // const updateUser = (id, user) => {
+  //   let data = new FormData();
+  //   data.append('file', user.image)
+  //   data.append('fname', user.fname)
+  //   data.append('lname', user.lname)
+  //   data.append('email', user.email)
+  //   axios.put(`/api/users/${id}`, data)
+  //     .then( res => {
+  //       setUser(res.data)
+  //       window.location.reload()
+  //     })
+  //     .catch( err => {
+  //       console.log(err)
+  //       setErrors({ 
+  //         variant: 'danger',
+  //         msg: err.response.data.errors[0]
+  //       })
+  //     })
+  // }
+
+  return (
+    <AuthContext.Provider value={{
+      user, 
+      setUser: (user) => setUser(user),
+      handleRegister,
+      handleLogin, 
+      handleLogout,
+      authenticated: user !== null,
+      errors, 
+      setErrors,
+      updateUser
+    }}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider;
---
+++ client/src/providers/PlantProvider.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export const PlantContext = React.createContext();
+
+export const PlantConsumer = PlantContext.Consumer;
+
+const PlantProvider = ({ children }) => {
+  const [plants, setPlants] = useState([])
+  const [errors, setErrors] = useState(null)
+  const navigate = useNavigate()
+
+  const getAllPlants = () => {
+    axios.get('/api/plants')
+      .then(res => setPlants(res.data))
+      .catch(err => {
+        setErrors({ 
+          variant: 'danger',
+          msg: err.response.data.errors[0]
+        })
+      })
+  }
+
+  const addPlant = (plant) => {
+    axios.post('/api/plants', { plant })
+      .then(res => setPlants([...plants, res.data]))
+      .catch(err => {
+        setErrors({ 
+          variant: 'danger',
+          msg: Object.keys(err.response.data.errors)[0] + " " + Object.values(err.response.data.errors)[0][0]
+        })
+      })
+  }
+
+  const updatePlant = (id, plant) => {
+    axios.put(`/api/plants/${id}`, { plant })
+      .then( res => {
+        const newUpdatedPlants = plants.map( p => {
+          if (p.id == id) {
+            return res.data 
+          }
+          return p
+        })
+        setPlants(newUpdatedPlants)
+        navigate('/plants')
+      })
+      .catch(err => {
+        setErrors({ 
+          variant: 'danger',
+          msg: Object.keys(err.response.data.errors)[0] + " " + Object.values(err.response.data.errors)[0][0]
+        })
+      })
+  }
+
+  const deletePlant = (id) => {
+    axios.delete(`/api/plants/${id}`)
+      .then(res => {
+        setPlants(plants.filter(p => p.id !== id))
+      })
+      .catch(err => {
+        setErrors({ 
+          variant: 'danger',
+          msg: err.response.data.errors[0]
+        })
+      })
+  }
+
+  // const getRandomPlant = () => {
+  //   axios.get('/api/randomcat')
+  //     .then(res => {
+  //       setRandomCat(res.data)
+  //     })
+  //     .catch(err => {
+  //       setErrors({ 
+  //         variant: 'danger',
+  //         msg: err.response.data.errors[0]
+  //       })
+  //     })
+  // }
+
+  return (
+    <PlantContext.Provider value={{
+      plants, 
+      errors, 
+      setErrors, 
+      getAllPlants,
+      addPlant,
+      updatePlant, 
+      deletePlant, 
+      // randomCat, 
+      // getRandomCat, 
+    }}>
+      { children }
+    </PlantContext.Provider>
+  )
+}
+
+export default PlantProvider;
