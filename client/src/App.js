034271a55ac41d036efb2e5be3e4b2ed9fca8a7a
Joy Nichols <joynichols@joys-air.lan>
2022-10-06 21:45:54 -0400

    comment out wishlist app.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,8 @@ const App = () => (
       <Route path='/' element={<ProtectedRoute /> }>
         <Route path='/plants' element={<Plants />} />
         <Route path='/:id/updatePlant' element={<PlantForm />} />
-        <Route path='/wishlists' element={<Wishlists />} />
-        <Route path='/:id/updateWishlist' element={<WishlistForm />} />
+        {/* <Route path='/wishlists' element={<Wishlists />} />
+        <Route path='/:id/updateWishlist' element={<WishlistForm />} /> */}
         <Route path='/:plantId/notes' element={<Notes />} />
         {/* <Route path='/profile' element={<Profile />} /> */}
       </Route>
