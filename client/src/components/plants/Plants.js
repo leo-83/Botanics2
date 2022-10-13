109646a984082be454eab7a411af04c21f0762a7
Austin <austinhill008@gmail.com>
2022-10-12 19:44:55 -0600

    random

--- client/src/components/plants/Plants.js
+++ client/src/components/plants/Plants.js
@@ -48,7 +48,11 @@ const Plants = ({ plants, getAllPlants }) => {
 
 const ConnectedPlants = (props) => (
   <PlantConsumer>
+<<<<<<< HEAD
     { value => <Plants {...props} {...value} /> }
+=======
+    { value => <Plants {...value} {...props} />}
+>>>>>>> 264e7c3 (commented out routes)
   </PlantConsumer>
 )
 
