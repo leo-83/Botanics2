6174e9cb3617795febaf776e5c4601ae56987647
Joy_N <103117755+jjilt@users.noreply.github.com>
2022-10-12 21:37:36 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,16 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Nomatch from './components/shared/Nomatch';
 import MainNavbar from './components/shared/MainNavbar';
-// import Footer from './components/shared/Footer';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import Plants from './components/plants/Plants';
+import PlantForm from './components/plants/PlantForm';
 import Profile from './components/auth/Profile';
+// import Notes from './components/notes/Notes';
+// import Wishlist from './components/wishlist/Wishlist'
+// import WishlistForm from './components/wishlist/WishlistForm'
+// import Footer from './components/shared/Footer';
+
 
 const App = () => (
   <>
@@ -15,22 +23,23 @@ const App = () => (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/' element={<ProtectedRoute /> }>
-        {/* <Route path='/plants' element={<Plants />} />
-        <Route path='/:id/updatePlant' element={<PlantForm />} /> */}
+      <Route path='/plants' element={<Plants />} /> 
+      <Route path='/:id/updatePlant' element={<PlantForm />} /> */}
         {/* <Route path='/wishlists' element={<Wishlists />} />
         <Route path='/:id/updateWishlist' element={<WishlistForm />} /> */}
         {/* <Route path='/:plantId/notes' element={<Notes />} /> */}
-        <Route path='/profile' element={<Profile />} />
+      <Route path='/profile' element={<Profile />} />
       </Route>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/*' element={<Nomatch />} />
-      <Route path='/:plantId/wishlists' element={ <Wishlists /> } />
     </Routes>
     </>
     </FetchUser>
     {/* <Footer /> */}
   </>
 )
+
+export default App;
 
 export default App;
