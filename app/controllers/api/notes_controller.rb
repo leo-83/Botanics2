ef31f4766e2d7ff71f8d31ebbafbc9ae45efa818
Christian Green <hoolio.suare@gmail.com>
2022-10-17 20:25:45 -0600

    fixed plant_id for notes

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -18,8 +18,30 @@ def create
     render json: { errors: @note.errors }, status: :unprocessable_entity
   end
 
-  def show
+def update
+  if @note.update(notes_params)
     render json: @note
+  else
+    render json: { errors: @note.errors }, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  @note.destroy
+  render json: { message: 'note deleted' }
+end
+
+private
+  def notes_params
+    params.require(:note).permit(:body)
+  end
+
+  def set_plant
+    @plant = Plant.find(params[:plant_id])
+  end
+
+  def set_note
+    @note = @plant.notes.find(params[:id])
   end
 
   def create
