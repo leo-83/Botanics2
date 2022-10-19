7c8b561a103791a27991fbec645f1ec082525d43
Austin <austinhill008@gmail.com>
2022-10-18 21:47:08 -0600

    about us page

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,46 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/shared/home/Home';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Nomatch from './components/shared/Nomatch';
-import MainNavbar from './components/shared/MainNavbar';
-import FetchUser from './components/auth/FetchUser';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import Plants from './components/plants/Plants';
-import PlantForm from './components/plants/PlantForm';
-import Profile from './components/auth/Profile';
-import Notes from './components/notes/Notes';
-import Wishlist from './components/wishlists/Wishlist';
-import WishlistForm from './components/wishlists/WishlistForm'
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/shared/home/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Nomatch from "./components/shared/Nomatch";
+import MainNavbar from "./components/shared/MainNavbar";
+import FetchUser from "./components/auth/FetchUser";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Plants from "./components/plants/Plants";
+import PlantForm from "./components/plants/PlantForm";
+import Profile from "./components/auth/Profile";
+import Notes from "./components/notes/Notes";
+import Wishlist from "./components/wishlists/Wishlist";
+import WishlistForm from "./components/wishlists/WishlistForm";
 // import Footer from './components/shared/Footer';
+import AboutUs from "./components/aboutus/AboutUs";
 
 const App = () => (
   <>
     <MainNavbar />
     <FetchUser>
       <>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/' element={<ProtectedRoute /> }>
-      <Route path='/plants' element={<Plants />} /> 
-      <Route path='/:id/updatePlant' element={<PlantForm />} />
-        <Route path='/wishlist' element={<Wishlist />} />
-        <Route path='/:id/updateWishlist' element={<WishlistForm />} />
-        <Route path='/wishlist' element={<Wishlist />} /> 
-        <Route path='/:id/updateWishlist' element={<WishlistForm />} />
-        <Route path='/:plantId/notes' element={<Notes />} />
-      <Route path='/profile' element={<Profile />} />
-      </Route>
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/*' element={<Nomatch />} />
-    </Routes>
-    </>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route path="/plants" element={<Plants />} />
+            <Route path="/:id/updatePlant" element={<PlantForm />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/:id/updateWishlist" element={<WishlistForm />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/:id/updateWishlist" element={<WishlistForm />} />
+            <Route path="/:plantId/notes" element={<Notes />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/*" element={<Nomatch />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+        </Routes>
+      </>
     </FetchUser>
     {/* <Footer /> */}
   </>
-)
+);
 
 export default App;
-
