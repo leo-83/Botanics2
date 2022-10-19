7e62e801b08de44a03002c358ac0969105f011c8
Leo Fuchs <leonardo.m.fuchs@gmail.com>
2022-10-18 18:13:58 -0600

    fix notes functionality

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -2,46 +2,46 @@ class Api::NotesController < ApplicationController
   before_action :set_plant
   before_action :set_note, only: [:show, :update, :destroy]
 
-def index
-  render json: @plant.notes
-end
+  def index
+    render json: @plant.notes
+  end
 
-def show
-  render json: @note
-end
-
-def create
-  @note = @plant.notes.new(note_params)
-  if @note.save
+  def show
     render json: @note
-  else
-    render json: { errors: @note.errors }, status: :unprocessable_entity
-  end
-end
-
-def update
-  if @note.update(note_params)
-    render json: @note
-  else
-    render json: { errors: @note.errors }, status: :unprocessable_entity
-  end
-end
-
-def destroy
-  @note.destroy
-  render json: { message: 'note deleted' }
-end
-
-private
-  def notes_params
-    params.require(:note).permit(:plant_id)
   end
 
-  def set_plant
-    @plant = Plant.find(params[:plant_id])
+  def create
+    @note = @plant.notes.new(note_params)
+    if @note.save
+      render json: @note
+    else
+      render json: { errors: @note.errors }, status: :unprocessable_entity
+    end
   end
 
-  def set_note
-    @note = @plant.notes.find(params[:id])
+  def update
+    if @note.update(note_params)
+      render json: @note
+    else
+      render json: { errors: @note.errors }, status: :unprocessable_entity
+    end
   end
+
+  def destroy
+    @note.destroy
+    render json: { message: 'note deleted' }
+  end
+
+  private
+    def note_params
+      params.require(:note).permit(:subject, :body)
+    end
+
+    def set_plant
+      @plant = Plant.find(params[:plant_id])
+    end
+
+    def set_note
+      @note = @plant.notes.find(params[:id])
+    end
 end
--- client/src/components/notes/NoteList.js
+++ client/src/components/notes/NoteList.js
@@ -1,15 +1,15 @@
 import { ListGroup, Container } from 'react-bootstrap';
 import NoteShow from './NoteShow';
 
-const NoteList = ({ notes, plant_id }) => (
+const NoteList = ({ notes, plantId }) => (
   <Container>
-    <h1>All Notes</h1>
+    <h1>All Notes For This Plant</h1>
     <ListGroup variant="flush">
       { notes.map( n => 
         <NoteShow 
           key={n.id}
           {...n}
-          plant_id={plant_id}
+          plantId={plantId}
         /> 
       )}   
     </ListGroup>
