d0f683b694bc86e37902c1cb7c8eee44c36fa2d2
Leo Fuchs <leonardo.m.fuchs@gmail.com>
2022-10-25 21:10:28 -0600

    update profile.js with myplants link

--- client/src/components/plants/MyPlants.js
+++ client/src/components/plants/MyPlants.js
@@ -1,10 +1,32 @@
-// import { Card, Modal, Button, Container, Row, Col, Image } from 'react-bootstrap';
-// import { PlantConsumer } from '../../providers/PlantProvider';
-// import { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import { MainHeader } from '../../styles/SharedStyles';
+import { Image, Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Slide } from 'react-awesome-reveal';
+import React from "react";
 
-// const PlantShow = ({ id, name, img, desc, deletePlant}) => {
-//   const [showing, setShow] = useState(false)
+const MyPlants = () => (
+  <>
+  <h1>
+    My Plants Page Coming Soon!
+  </h1>
+  <div style={{ 
+      backgroundImage: `url("https://images.pexels.com/photos/2778192/pexels-photo-2778192.jpeg?auto=compress&cs=tinysrgb&w=600")` 
+    }}>
+    </div>
+    <Container>
+      <Row >
+        <Col md='2' sm='20' >
+          <Slide 
+            direction='center'
+          >
+          
+          </Slide>
+        </Col>
+      </Row>
+    </Container>
+    {/* </div> */}
+  </>
+)
 
 //   return (
 //     <>
