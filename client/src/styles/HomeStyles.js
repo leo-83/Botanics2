b76d3d663bc21c9030979a13d31f0db10f5c76cf
Christian Green <hoolio.suare@gmail.com>
2022-10-25 21:34:56 -0600

    home carousel and text

--- client/src/styles/HomeStyles.js
+++ client/src/styles/HomeStyles.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 
+const Home = styled.h1`
+ background: linear-gradient(to bottom right, aliceblue, black);
+ font-size: medium;
+ text-align: center;
+`
 
 export const MainTitle = styled.h1`
   text-align: center;
-  font-size: 34px;
+  font-size: 24px;
   font-weight: 200;
 `
 
 export const CarouselImg = styled.img`
-  height:300px; 
-  width:300px;
-  align-content: center;
+  height:573px; 
+  width:572px;
 
 `
 
