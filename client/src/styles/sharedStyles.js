6fc04aa49bb0d0332269508531918c0d82c59d4c
Leo Fuchs <leonardo.m.fuchs@gmail.com>
2022-10-26 14:10:51 -0600

    shared styles added?

--- client/src/styles/sharedStyles.js
+++ client/src/styles/sharedStyles.js
@@ -1,25 +1,51 @@
 import styled from 'styled-components';
+import { Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+const whatFont = (size) => {
+  switch (size) {
+    case 'sm':
+      return '18px';
+    case 'md':
+      return '28px'
+    default:
+      return '48px';
+    
+  }
+}
+
+
 
 export const MainHeader = styled.h1`
-  color: black;
-  background: #FFFFFF;
-  font-size: 45px;
-  text-align: left;
-`
-
-export const Header2 = styled.h2`
-
-`
-
-export const Button = styled.button`
-  color: #FFFFFF;
-  background: #248232;
+  color: #248232;
+  font-size: ${ props => whatFont(props.size) };
+  background: #605856;
   text-align: center;
-  border-width: 0px;
-  border-radius: 25px;
-  height: 50px;
-  width: 80px;
-  font-family: andale mono, monospace;
-
-  /* outline-width: 10px; */
 `
+export const GreyCol = styled(Col)`
+  background: #605856;
+  color: #FFFFFF;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  `
+
+export const WhiteCol = styled(Col)`
+  background: #FFFFFF;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  `
+export const FlexDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  `
+
+export const GreenLink = styled (Link)`
+  color: #008000; 
+  font-size: 24px;
+  `
+ 
