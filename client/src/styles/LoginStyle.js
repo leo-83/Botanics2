3c6c80f07617ab28a3c863796b7c063529731f5e
Leo Fuchs <leonardo.m.fuchs@gmail.com>
2022-10-27 21:11:23 -0600

    login and register image and styling

---
+++ client/src/styles/LoginStyle.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+import { Card, Image } from 'react-bootstrap';
+
+export const imageContainer = styled.div `
+   background-color: #5F9EA0;
+   scale: 80%;
+`
+export const imageObject = styled.image `
+   padding: 0%;
+   margin: -0% 0% 0% 0%;
+   scale: 40%;
+`
+
+export const imageTextRow = styled.div `
+   margin: -10% 5% 10% 5%;
+   color: #000000;
+`
